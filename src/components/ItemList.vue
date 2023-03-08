58edb98bbf59b39a87b37714ed609a5815e74e85
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-03-08 11:18:31 +0630

    reduce stock

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -5,8 +5,9 @@
         <div>{{ item.name }}</div>
         <q-item-label overline>Stock : {{ item.stock }}</q-item-label>
       </q-item-section>
-      <q-item-section top side>
-        <q-icon name="edit" @click="showEditItemFormDialog(item)" />
+      <q-item-section top side class="q-gutter-y-sm">
+        <q-btn icon="edit" @click="showEditNameDialog(item)" />
+        <q-btn icon="eject" @click="showReduceStockDialog(item)" />
       </q-item-section>
     </q-item>
   </q-list>
@@ -25,7 +26,7 @@ const props = defineProps({
 const emit = defineEmits(["itemUpdated"]);
 const { dialog } = useQuasar();
 const { api } = useUtil();
-const showEditItemFormDialog = (item) => {
+const showEditNameDialog = (item) => {
   dialog({
     noBackdropDismiss: true,
     position: "top",
@@ -41,6 +42,33 @@ const showEditItemFormDialog = (item) => {
       url: "items/" + item.id,
       data: {
         name: val,
+      },
+    }).then(({ data }) => {
+      emit("itemUpdated", data.item);
+    });
+  });
+};
+
+const showReduceStockDialog = (item) => {
+  dialog({
+    noBackdropDismiss: true,
+    position: "top",
+    cancel: true,
+    title: `Reduce stock of item ${item.name}`,
+    prompt: {
+      type: "number",
+      inputmode: "numeric",
+      pattern: "[0-9]*",
+      model: "",
+      isValid: (val) => val > 0 && val <= item.stock,
+      hint: item.stock + " Remaining",
+    },
+  }).onOk((val) => {
+    api({
+      method: "POST",
+      url: `items/${item.id}/reduce-stock`,
+      data: {
+        quantity: val,
       },
     }).then(({ data }) => {
       emit("itemUpdated", data.item);
